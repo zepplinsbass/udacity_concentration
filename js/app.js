a565979ce35aea3051fb483f2b75d4d2efe54a54
zeppelinsbass <mlassodev@gmail.com>
2018-05-07 11:28:20 -0400

    First draft of the game. Still building the basic requirements.

--- js/app.js
+++ js/app.js
@@ -10,21 +10,125 @@
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+function Card (name, status) {
+    this.name = name;
+    this.status = status;
 }
 
+/* The game object contains the deck array (an array of Card objects), the
+ * cardElems array (the NodeList of html card elements), and the checkCard
+ * property, which always contains one of the Card objects from the deck. It
+ * also contains the flipCard and checkMatch methods, which run in the
+ * EventListener for each card.*/
+
+const game = {
+    deck: [],
+    cardA: null,
+    cardB: null,
+    timer: '',
+    score: 0,
+    starRating: 0,
+    moves: 0,
+}
+
+function playDeck () {
+
+    function createCards () {
+        const cardList = document.body.querySelector('.deck');
+        const cardStatus = cardList.getElementsByTagName('li');
+        for (let i = 0; i < cardStatus.length; i++) {
+            const cardName = cardStatus[i].getElementsByTagName('i');
+            game.deck[i] = new Card(cardName[0].className, cardStatus[i].className);
+
+            //Adds the event listener to each card.
+            cardStatus[i].addEventListener('click', function () {
+                  checkMatch(game.deck[i], cardStatus[i]);
+            });
+        }
+    }
+
+    /* Shuffle the deck array. Shuffle function from http://stackoverflow.com/a/2450976 */
+    function shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+            }
+        return array;
+    }
+
+    function createDeck () {
+        createCards();
+        shuffle(game.deck);
+        const list = document.body.querySelector('.deck');
+        const elem = list.getElementsByTagName('li');
+        for (let i = 0; i < game.deck.length; i++) {
+            const name = elem[i].getElementsByTagName('i');
+            elem[i].className = 'card';
+            game.deck[i].status = 'card';
+            name[0].className = game.deck[i].name
+        }
+    }
+
+    function checkMatch (card, elem) {
+        if (game.cardA === null) {
+            game.cardA = card;
+            elem.className = 'card open show';
+            game.cardA.status = 'card open show';
+            return;
+        } else if ((game.cardA !== null) && (game.cardA !== card)) {
+            game.cardB = card;
+            elem.className = 'card open show';
+            game.cardB.status = 'card open show';
+
+            //move count is updated
+            game.moves++;
+            //TODO update html
+            //TODO update Star Rating
+        } else return;
+
+        if (game.cardA.name === game.cardB.name) {
+            isMatch();
+        } else {
+            flipDown();
+        }
+    }
+
+    function isMatch () {
+        game.score++;
+        if (game.score === game.deck.length) {
+            gameOver();
+        } else {
+        const list = document.body.querySelector('.deck');
+        const match = list.querySelectorAll('li.card.open.show');
+        match[0].className = 'card match';
+        match[1].className = 'card match';
+        //TODO remove event listeners
+        }
+        game.cardA = null;
+        game.cardB = null;
+    }
+
+    function flipDown () {
+        setTimeout(function () {
+            const list = document.body.querySelector('.deck');
+            const mixed = list.querySelectorAll('li.card.open.show');
+            mixed[0].className = 'card';
+            mixed[1].className = 'card';
+
+            game.cardA = null;
+            game.cardB = null;
+        }, 800);
+    }
+
+createDeck();
+
+}
+
+playDeck();
 
 /*
  * set up the event listener for a card. If a card is clicked:
